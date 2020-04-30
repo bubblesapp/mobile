eaf86dc17c917c02054968dde0a46970f96a3361
Edouard Goossens <edouard@woojo.ooo>
2020-04-30 19:04:59 +0200

    Remove

--- src/api/FirebaseAPI.web.ts
+++
@@ -1,9 +0,0 @@
-import {FirebaseAPI} from '@bubblesapp/api';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyB1uHwOarBrIvaSYnMgYxnoH1UKWGkIiUc', //process.env.REACT_APP_FIREBASE_API_KEY,
-  projectId: 'bubbles-273e5', //process.env.REACT_APP_FIREBASE_PROJECT_ID,
-};
-export const api = new FirebaseAPI(firebase.initializeApp(firebaseConfig));
